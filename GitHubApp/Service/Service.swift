61bc91a6a883c826d0eae326f5fda95880d3b8cf
All3yp <noyallepereira@gmail.com>
2023-02-16 22:26:57 -0300

    Feat: Created URL fetchList and call performGet method

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -8,8 +8,22 @@ struct Service {
         self.network = network
     }
 
-    func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
-        
-        // TODO
+  func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
+    guard let url = URL(string:"https://api.github.com/users/\(user)/repos") else {
+      return
     }
+
+    network.performGet(url: url) { data in
+      guard let data = data else {
+        return completion(nil)
+      }
+      do {
+        let repositories = try JSONDecoder().decode([Repository].self, from: data)
+        completion(repositories)
+      } catch {
+        print(error.localizedDescription)
+        completion(nil)
+      }
+    }
+  }
 }
